f3650303e459d7ce52e708b4853f54cc67669a79
Kauã Steliano <stelianok@gmail.com>
2021-12-09 21:56:14 -0300

    An additional confirmation is now required for deleting tasks.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,9 +39,26 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    setTasks(oldState => oldState.filter(
-      task => task.id !== id
-    ));
+    Alert.alert(
+      "Remover item",
+      "Tem certeza que você deseja remover esse item?",
+      [
+        {
+          text: "Não",
+          style: "cancel"
+        },
+        {
+          text: "Sim",
+          onPress: () => {
+            setTasks(oldState => oldState.filter(
+              task => task.id !== id
+            ));
+          },
+          style: "default",
+        }
+      ]
+    )
+
   }
 
   return (
